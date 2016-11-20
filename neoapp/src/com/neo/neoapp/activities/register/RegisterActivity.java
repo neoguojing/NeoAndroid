35e2bb99ac7193fc3052929e1b21428733146a76
neoguojing <guojing_neo@163.com>
2016-11-20 08:24:02 +0800

    remove a unused import

--- neoapp/src/com/neo/neoapp/activities/register/RegisterActivity.java
+++ neoapp/src/com/neo/neoapp/activities/register/RegisterActivity.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.app.NotificationCompat.WearableExtender;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
